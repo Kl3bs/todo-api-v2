28a12b8968c4b2bdf9f16ceecaf225c4596f0229
Kleber Vasconcelos <klebervasconcelos@Macbook-Pro.local>
2024-01-31 01:08:26 -0300

    cors update

--- backend/settings.py
+++ backend/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -152,3 +154,6 @@ LOGGING = {
         'level': 'DEBUG',  # Adjust the level as needed
     },
 }
+
+
+CORS_ORIGIN_ALLOW_ALL = True
